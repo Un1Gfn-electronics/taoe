40a5239bd7776d6b464e94d96406a210d9f469bb
Darren Ng <darren19970810@gmail.com>
2021-02-16 17:35:43 +0800

    LED model from unknown source

--- p14_itstoohot.cmd
+++ p14_itstoohot.cmd
@@ -3,11 +3,11 @@ Vctrl ctrl 0 DC 0
 
 .control
 
-  save ctrl @R5[i] ocout
+  save ctrl @D1[id] ocout
 
   dc Vctrl 11k 6k -1
 
-  plot @R5[i] vs ctrl
+  plot -@D1[id] vs ctrl
 
   plot ocout vs ctrl
   + ylimit 0 5
