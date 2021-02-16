0eab66ca9bae3294ac762b6814cd4f6c204c6cf3
Darren Ng <darren19970810@gmail.com>
2021-02-16 16:57:15 +0800

    fix negative vincr

--- p14_itstoohot.cmd
+++ p14_itstoohot.cmd
@@ -1,12 +1,13 @@
-* It's too hot simulation
-
 .GLOBAL ctrl
 Vctrl ctrl 0 DC 0
 
 .control
 
-  save @R5[i]
-  dc Vctrl 11k 7k 1
+  save @R5[i] ctrl
+
+  dc Vctrl 11k 7k -1
+
   plot @R5[i] vs ctrl
+  * plot ctrl vs ctrl
 
 .endc
