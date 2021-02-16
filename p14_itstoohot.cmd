ca401f79b83fd0c6f1a27d2fcf23d4593eb18d1c
Darren Ng <darren19970810@gmail.com>
2021-02-16 17:22:30 +0800

    I & V

--- p14_itstoohot.cmd
+++ p14_itstoohot.cmd
@@ -3,11 +3,13 @@ Vctrl ctrl 0 DC 0
 
 .control
 
-  save @R5[i] ctrl
+  save ctrl @R5[i] ocout
 
-  dc Vctrl 11k 7k -1
+  dc Vctrl 11k 6k -1
 
   plot @R5[i] vs ctrl
-  * plot ctrl vs ctrl
+
+  plot ocout vs ctrl
+  + ylimit 0 5
 
 .endc
